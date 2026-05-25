3467e2b7f34eebb34be28812f568014a0fefffea
leoncol <camiloleon196@gmail.com>
2026-05-25 14:06:57 -0500

    Gameboard places ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -22,27 +22,70 @@ function Gameboard() {
         return newShip;
     }
 
-    let placeShip = function(size, coord1, coord2){
+    let isInBounds = function(length, coord1, coord2, orientation){ // Check if the subsequent vertical or horizontal slots are in bounds
+        if (length == 2 && orientation == 'h' && newBoard[coord1][coord2+1] === null){
+            return true; // check size 2 horizontally
+        } else if (length == 2 && orientation == 'v' && newBoard[coord1+1] !== undefined){
+            return true; // check size 2 vertically
+        } else if (length == 3 && orientation == 'h' && newBoard[coord1][coord2+1] === null && newBoard[coord1][coord2+2] === null ){
+            return true; //check size 3 horizontally
+        } else if (length == 3 && orientation == 'v' && newBoard[coord1+1] !== undefined && newBoard[coord1+2] !== undefined ){
+            return true; //check size 3 vertically
+        } else if (length == 4 && orientation == 'h' && newBoard[coord1][coord2+1] === null && newBoard[coord1][coord2+2] === null && newBoard[coord1][coord2+3]  === null){
+            return true; //check size 4 horizontally    
+        } else if (length == 4 && orientation == 'v' && newBoard[coord1+1] !== undefined && newBoard[coord1+2] !== undefined && newBoard[coord1+3] !== undefined){
+            return true; //check size 4 vertically    
+        } else {
+            return false;
+        }
+    }
+
+
+    let placeShip = function(size, coord1, coord2, orientation){
         if (coord1 > 9|| coord2 > 9 ){
             return 'Invalid coords'
         } else if (size > 4){
             return 'Invalid size'
-        }{
+        } else if (size != 1 && orientation != 'h' && orientation != 'v'){
+            return 'Invalid orientation' // test orientation --  evaluate when ship is out of bounds
+        } else {
             let newShip = createShip(size);
             if (newShip.returnLength() == 1){
                 newBoard[coord1][coord2] = newShip;
                 return newBoard[coord1][coord2];
-            } else if (newShip.returnLength() == 2){
+            } else if (newShip.returnLength() == 2 && orientation == 'h' && isInBounds(size, coord1, coord2, orientation)){
                 newBoard[coord1][coord2] = newShip;
                 newBoard[coord1][coord2+1] = newShip;
                 return [newBoard[coord1][coord2],newBoard[coord1][coord2+1]];
-            } else if (newShip.returnLength() == 3){
+            } else if (newShip.returnLength() == 2 && orientation == 'v' && isInBounds(size, coord1, coord2, orientation) == true){
+                newBoard[coord1][coord2] = newShip;
+                newBoard[coord1+1][coord2] = newShip;
+                return [newBoard[coord1][coord2],newBoard[coord1+1][coord2]];
+            } else if (newShip.returnLength() == 3 && orientation == 'h' && isInBounds(size, coord1, coord2, orientation) == true){
+                newBoard[coord1][coord2] = newShip;
+                newBoard[coord1][coord2+1] = newShip;
+                newBoard[coord1][coord2+2] = newShip;
+                return [newBoard[coord1][coord2], newBoard[coord1][coord2+1], newBoard[coord1][coord2+2]];
+            } else if (newShip.returnLength() == 3 && orientation == 'v' && isInBounds(size, coord1, coord2, orientation) == true){
                 newBoard[coord1][coord2] = newShip;
                 newBoard[coord1+1][coord2] = newShip;
                 newBoard[coord1+2][coord2] = newShip;
-                return [newBoard[coord1][coord2], newBoard[coord1+1][coord2], newBoard[coord1+2][coord2]]
+                return [newBoard[coord1][coord2], newBoard[coord1+1][coord2], newBoard[coord1+2][coord2]];
+            }  else if (newShip.returnLength() == 4 && orientation == 'h' && isInBounds(size, coord1, coord2, orientation) == true){
+                newBoard[coord1][coord2] = newShip;
+                newBoard[coord1][coord2+1] = newShip;
+                newBoard[coord1][coord2+2] = newShip;
+                newBoard[coord1][coord2+3] = newShip;
+                return [newBoard[coord1][coord2], newBoard[coord1][coord2+1], newBoard[coord1][coord2+2],newBoard[coord1][coord2+3]];
+            }   else if (newShip.returnLength() == 4 && orientation == 'v' && isInBounds(size, coord1, coord2, orientation) == true){
+                newBoard[coord1][coord2] = newShip;
+                newBoard[coord1+1][coord2] = newShip;
+                newBoard[coord1+2][coord2] = newShip;
+                newBoard[coord1+3][coord2] = newShip;
+                return [newBoard[coord1][coord2], newBoard[coord1+1][coord2], newBoard[coord1+2][coord2],newBoard[coord1+3][coord2] ];
+            } else {
+                return 'Invalid placement. Ship out of bounds.'
             }
-            
         }
     }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -31,21 +31,80 @@ test('Check the length of a placed ship ', () => {
 })
 
 
-test('Place a size 2 ship at a specific coord ', () => {
+test('Place a size 2 horizontal ship at a specific coord ', () => {
     let newGameboard = gameboard();
-    let object = newGameboard.placeShip(2,0,0)
+    let object = newGameboard.placeShip(2,0,0,'h')
     
     expect(typeof object[0]).toBe('object');
     expect(typeof object[1]).toBe('object');
     expect(object).not.toBeNull(); 
 })
 
+test('Place a size 2 vertical ship at a specific coord ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(2,0,0, 'v')
+    
+    expect(typeof object[0]).toBe('object');
+    expect(typeof object[1]).toBe('object');
+    expect(object).not.toBeNull(); 
+})
+
+
 test('Place a size 3 vertical ship at a specific coord ', () => {
     let newGameboard = gameboard();
-    let object = newGameboard.placeShip(3,7,6)
+    let object = newGameboard.placeShip(3,7,6, 'v')
     
     expect(typeof object[0]).toBe('object');
     expect(typeof object[1]).toBe('object');
     expect(typeof object[2]).toBe('object');
     expect(object).not.toBeNull(); 
+})
+
+test('Place a size 2 horizontal ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(2,0,9,'h')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
+})
+
+test('Place a size 2 vertical ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(2,9,0,'v')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
+})
+
+test('Place a size 3 horizontal ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(3,0,8,'h')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
+})
+
+test('Place a size 3 vertical ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(3,8,0,'v')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
+})
+
+test('Place a size 4 horizontal ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(4,0,7,'h')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
+})
+
+
+test('Place a size 4 vertical ship at a specific coord out of bounds ', () => {
+    let newGameboard = gameboard();
+    let object = newGameboard.placeShip(4,0,7,'h')
+    
+    
+    expect(object).toEqual('Invalid placement. Ship out of bounds.')
 })
